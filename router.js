34167e106b5876211338b3f11828d1cdaf3cb4d7
Marcel Louwrens <mlouwrensm@gmail.com>
2020-07-23 17:46:49 +0200

    sse test data

--- router.js
+++ router.js
@@ -15,4 +15,36 @@ module.exports = function(app) {
 
 	app.post('/signup', Authenticaton.signup);
 
+	app.get('/dashboard', function(req, res) {
+	  res.writeHead(200, {
+	  	Connection: "keep-alive",
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Access-Control-Allow-Origin": "*",
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+        'Access-Control-Allow-Headers': 'X-Requested-With,content-type,Accept',
+	  })
+	  countdown(res, 12)
+	})
+
+	function countdown(res, count) {
+
+	  let id = Math.floor(Math.random() * Math.floor(count));
+	  let randomStatusId = Math.floor(Math.random() * Math.floor(3));
+
+	  let connections = ["not connected", "error", "connected"];
+	  let activities = ["idle", "transmitting", "receiving"];
+
+	  const obj = JSON.stringify({ circuit_id: id, action: "update_circuit_status", value: connections[randomStatusId] });
+	  const obj2 = JSON.stringify({ circuit_id: id, action: "update_circuit_activity", value: activities[randomStatusId] });
+	
+	  res.write("data: "+obj+"\n\n");
+	  res.write("data: "+obj2+"\n\n");
+	  res.flush();
+	  if (count > 1)
+	    setTimeout(() => countdown(res, count-1), 1000)
+	  else
+	    res.end()
+	}
+
 }
